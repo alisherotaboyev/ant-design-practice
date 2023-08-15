db2a03334794ba43f8172fc47e35c371ede822c3
alisher <alisherotaboyev072@gmail.com>
2023-08-15 13:21:52 +0500

    first commit

---
+++ src/components/Registration/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import * as Style from "./style";
+import { Container } from "../Container/style";
+
+const Registration = () => {
+  return (
+    <Style.Register>
+      <Container>
+        <Style.RegisterWrapper></Style.RegisterWrapper>
+      </Container>
+    </Style.Register>
+  );
+};
+
+export default Registration;
---
+++ src/components/Registration/style.js
@@ -0,0 +1,16 @@
+import { styled } from "styled-components";
+
+const Register = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const RegisterWrapper = styled.div`
+  width: 100%;
+  height: 950px;
+  background-color: red;
+`;
+
+export { Register, RegisterWrapper };
